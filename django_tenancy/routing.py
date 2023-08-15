7db7f8a7bac4e51f638bfedf975a695848dbfb61
danielwood <gunnerwoody449@gmail.com>
2023-08-15 07:45:05 +0100

    Backend:  change url patterns to one list.

--- django_tenancy/routing.py
+++ django_tenancy/routing.py
@@ -2,11 +2,8 @@ from django.urls import re_path
 from . import consumers
 
 
-prod_socket_urlpatterns =  [
+websocket_urlpatterns =  [
     
-    #re_path(r"ws/issues/support/(?P<teamName>\w+)/(?P<user>[\w.%+-]*@[A-Za-z0-9.-]*\.[A-Za-z]{0,4}|AnonymousUser)/$", consumers.SupportConsumerASync.as_asgi()),
     re_path(r"wss/issues/support/(?P<teamName>\w+)/(?P<user>[\w.%+-]*@[A-Za-z0-9.-]*\.[A-Za-z]{0,4}|AnonymousUser)/$", consumers.SupportConsumerASync.as_asgi()),
     
 ]
-
-websocket_urlpatterns = prod_socket_urlpatterns
