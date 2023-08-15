a901ece9d73523451fb41821d21f887606fc7081
danielwood <gunnerwoody449@gmail.com>
2023-08-15 09:18:06 +0100

    Backend:  register to do in admin.py.

--- apps/core/todo/admin.py
+++ apps/core/todo/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Task, Assignment, Reminder, Category, Group
 
-# Register your models here.
+admin.site.register(Task)
+admin.site.register(Assignment)
+admin.site.register(Reminder)
+admin.site.register(Category)
+admin.site.register(Group)
+
