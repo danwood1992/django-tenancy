b45553944b4e88c70589253f4d48346afbb34279
danielwood <gunnerwoody449@gmail.com>
2023-08-19 12:09:09 +0100

    Frontend:  develop hero.

---
+++ django_realms-frontend/src/components/Logo.tsx
@@ -0,0 +1,16 @@
+export function Logo(props: React.ComponentPropsWithoutRef<'svg'>) {
+  return (
+    <svg width="500" height="100" viewBox="0 0 500 100" {...props}>
+      {/* Text */}
+      <text x="20" y="70" font-family="Arial" font-size="50" font-weight="bold" fill="#4ade80">
+        Django Realms
+      </text>
+      {/* Inner circles representing multiple realms or tenants with different colors */}
+      <circle cx="410" cy="35" r="10" fill="#2D3748"/> {/* Slate color */}
+      <circle cx="425" cy="50" r="10" fill="#4ade80"/> {/* Green color */}
+      <circle cx="440" cy="35" r="10" fill="#2D3748"/> {/* Slate color */}
+      <circle cx="410" cy="65" r="10" fill="#4ade80"/> {/* Green color */}
+      <circle cx="440" cy="65" r="10" fill="#2D3748"/> {/* Slate color */}
+    </svg>
+  )
+}
